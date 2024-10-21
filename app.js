d838c39c2181ec295525f2fa9ecae1eec6fdb375
@sjkim2 <>
2024-10-21 12:43:16 +0900

    add commends

--- app.js
+++ app.js
@@ -5,48 +5,60 @@ const { exec } = require('child_process');
 const app = express();
 const port = 8887;
 
-// Body-parser 설정
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// POST 요청을 통해 셸 스크립트 실행
-app.post('/cmd', (req, res) => {
-    const rootPath = "cd /data/lucida-for-docker/polestar/bin && "
-    const command = rootPath + req.body.text; // Slack Slash Command에서 전달된 명령어 또는 스크립트 이름
+// Root path where scripts are located
+const rootPath = "cd /data/lucida-for-docker/polestar/bin && ";
 
-    if (!command) {
-      return res.status(400).send({
-        response_type: 'ephemeral',
-        text: '실행할 명령어 또는 스크립트 이름을 입력하세요.'
-      });
-    }
-  
+// Helper function to execute SSH command
+const executeSSHCommand = (command, res) => {
     const sshCommand = `ssh -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@192.168.213.191 "${command}"`;
 
-    // 명령어 실행
     exec(sshCommand, { shell: '/bin/bash' }, (error, stdout, stderr) => {
-      if (error) {
-        console.error(error.message)
-        return res.status(500).send({
-          response_type: 'in_channel',
-          text: `Error: ${error.message}`
-        });
-      }
+        if (error) {
+            return res.send({
+                response_type: 'in_channel',
+                text: `Error: ${error.message}`
+            });
+        }
 
-      if(stderr) {
+        if (stderr) {
+            return res.send({
+                response_type: 'in_channel',
+                text: `stderr: ${stderr}`
+            });
+        }
+
         return res.send({
             response_type: 'in_channel',
-            text: `${stderr}`
-          });
-      }
-
-      return res.send({
-        response_type: 'in_channel',
-        text: `${stdout}`
-      });
+            text: `stdout: ${stdout}`
+        });
     });
-  });
-  
-  app.listen(port, () => {
-    console.log(`run on ${port}`);
-  });
+};
+
+// Docker pull endpoint
+app.post('/docker/pull/:service_name', (req, res) => {
+    const serviceName = req.params.text;
+    const command = `${rootPath} ./script-domain.sh pull ${serviceName}`;
+    executeSSHCommand(command, res);
+});
+
+// Docker up endpoint
+app.post('/docker/up/:service_name', (req, res) => {
+    const serviceName = req.params.text;
+    const command = `${rootPath} ./script-domain.sh up ${serviceName}`;
+    executeSSHCommand(command, res);
+});
+
+// Docker logs endpoint
+app.post('/docker/logs/:service_name', (req, res) => {
+    const serviceName = req.params.text;
+    const command = `${rootPath} docker logs ${serviceName}`;
+    executeSSHCommand(command, res);
+});
+
+// Server start
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
